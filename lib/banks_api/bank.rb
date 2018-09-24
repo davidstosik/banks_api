bd26716de7c1b614c7555edec9003d667fad888a
David Stosik <david.stosik+git-noreply@gmail.com>
2018-09-25 08:37:08 +0900

    First working version for Shinsei

---
+++ lib/banks_api/bank.rb
@@ -0,0 +1,36 @@
+require "singleton"
+require "monitor"
+
+module BanksApi
+  class Bank
+    include Singleton
+    extend SingleForwardable
+
+    def_single_delegators :instance, :register_bank, :get_bank_api
+
+    def initialize
+      @registry = {}
+      @mutex = Monitor.new
+    end
+
+    def register_bank(bank_name, api_class)
+      with_mutex do
+        registry[bank_name.to_s] = api_class
+      end
+    end
+
+    def get_bank_api(bank_name)
+      with_mutex do
+        registry.fetch(bank_name.to_s)
+      end
+    end
+
+    private
+
+      attr_reader :registry, :mutex
+
+      def with_mutex
+        mutex.synchronize { yield }
+      end
+  end
+end
---
+++ lib/banks_api/user_account.rb
@@ -0,0 +1,24 @@
+module BanksApi
+  class UserAccount
+    def initialize(bank_name, credentials)
+      @bank_name = bank_name
+      @credentials = credentials
+    end
+
+    def accounts
+      @accounts ||= api.fetch_accounts
+    end
+
+    private
+
+      attr_reader :bank_name, :credentials
+
+      def api
+        @_api ||= api_class.new(credentials)
+      end
+
+      def api_class
+        Bank.get_bank_api(bank_name)
+      end
+  end
+end
