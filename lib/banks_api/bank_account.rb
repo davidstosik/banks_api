1fb8271710dab7e1cfab9a6d972ec3cea60b583b
David Stosik <david.stosik+git-noreply@gmail.com>
2018-09-27 13:17:44 +0900

    Introduce the Money gem to handle amounts

--- lib/banks_api/bank_account.rb
+++ lib/banks_api/bank_account.rb
@@ -1,16 +1,17 @@
+require "money"
 require "banks_api/inspectable_builder"
 
 module BanksApi
   class BankAccount
-    include InspectableBuilder.new(%i(id description currency balance))
+    include InspectableBuilder.new(%i(id description balance))
 
     attr_reader :id, :description, :currency, :balance
 
     def initialize(id:, description:, currency:, balance:)
       @id = id
       @description = description
-      @currency = currency
-      @balance = balance
+      @currency = Money::Currency.find(currency)
+      @balance = Money.from_amount(balance, @currency)
     end
   end
 end
--- lib/banks_api/transaction.rb
+++ lib/banks_api/transaction.rb
@@ -2,15 +2,14 @@ require "banks_api/inspectable_builder"
 
 module BanksApi
   class Transaction
-    include InspectableBuilder.new(%i(date description amount currency))
+    include InspectableBuilder.new(%i(date description amount))
 
-    attr_reader :date, :description, :amount, :currency
+    attr_reader :date, :description, :amount
 
     def initialize(date:, description:, amount:, currency:)
       @date = date
       @description = description
-      @amount = amount
-      @currency = currency
+      @amount = Money.from_amount(amount, currency)
     end
   end
 end
